3d973a3308936801bb34bb4b33f99cc0c6c4fddb
Kavia AI <production@kavia.ai>
2026-02-13 12:11:19 +0000

    Checkpoint 1 - cg9ad2f2cb

---
+++ lms_backend/src/main/java/com/example/demo/lmsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lmsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(lmsbackendApplication.class, args);
+	}
+
+}
